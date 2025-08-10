9493cc80d9eca374d473ef2a91441589eb2746b0
Alumno <alumno@cepibase.int>
2025-08-10 12:38:39 +0200

    he añadido dos funciones

--- principal.py
+++ principal.py
@@ -5,10 +5,11 @@ from pymongo import MongoClient
 from bson import ObjectId
 from flask_mail import Mail, Message
 
-def comprueba_fondo():
+def usuario():
     pass
 
-DOCUMENTOS = ["doc", "docx"]
+def password():
+    pass
 
 EXTENSIONES = ["png", "jpg", "jpeg"]
 app = Flask(__name__)
